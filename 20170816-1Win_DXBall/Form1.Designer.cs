996993b5b5a8312b2b8249e53a947c710a011678
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-28 20:50:24 +0300

    created at 16.08.2014 (dd.mm.yyyy)

---
+++ 20170816-1Win_DXBall/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _20170816_1Win_DXBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.top = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // top
+            // 
+            this.top.AutoSize = true;
+            this.top.Checked = true;
+            this.top.Location = new System.Drawing.Point(41, 36);
+            this.top.Name = "top";
+            this.top.Size = new System.Drawing.Size(14, 13);
+            this.top.TabIndex = 0;
+            this.top.TabStop = true;
+            this.top.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 448);
+            this.Controls.Add(this.top);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton top;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 20170816-1Win_DXBall/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20170816_1Win_DXBall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        int X = 3,Y = 3;
+
+        // formun genişliği ve yüksekliği hesaplanırken çerçeveler dahil edilmektedir. (height,width)
+        //Eğer elemanın X değerini formun yüksekliğine göre kıyaslarsak daha farklı bir noktada bulunur.
+        //çerçeveler hariç kullanılan alanı almak için CLIENTSIZE özelliğini kullanabiliriz. böylece çerçeveler dahil olmadan hesaplanmış olan yükseklik ve genişlik değerlerini alırız.
+
+            /*
+             BU TÜR İŞLEMLER İÇİN WPF KULLANMAK DAHA İYİ BİR SONUÇ ELDE ETMEMİZİ SAĞLAR.
+             WPF : GÖRÜNTÜYÜ OLUŞTURMAK İÇİN GPU KULLANIR
+             WİNFORM : CPU KULLANIR.
+             */
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            top.Left += X;
+            top.Top += Y;
+
+            if (top.Left <= 0 || top.Right >= this.ClientSize.Width)
+            {
+                X *= -1;
+            }
+
+            if (top.Top <= 0 || top.Bottom >= this.ClientSize.Height)
+            {
+                Y *= -1;
+            }
+            
+        }
+    }
+}
